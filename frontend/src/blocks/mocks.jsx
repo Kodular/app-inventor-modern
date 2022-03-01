ea4da2da219f2e868b04b621d218ce3f2ed52f25
Pavitra Golchha <hello@pavi2410.me>
2022-03-01 16:51:15 +0530

    layout editor dnd

---
+++ frontend/src/blocks/mocks.jsx
@@ -0,0 +1,105 @@
+import { Button, Group, Input } from "@mantine/core"
+import { useListState } from "@mantine/hooks"
+import { useRef } from "react"
+import { useDrag, useDrop } from "react-dnd"
+
+export const mocks = {
+  'button': MockButton,
+  'input': MockInput,
+  'row': MockRow,
+  'column': MockColumn,
+}
+
+function MockButton({ item }) {
+  const [{ isDragging }, drag] = useDrag({
+    type: "component",
+    item,
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+  return (
+    <Button ref={drag}>Button</Button>
+  )
+}
+
+function MockInput({ item }) {
+  const [{ isDragging }, drag] = useDrag({
+    type: "component",
+    item,
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+  return (
+    <Input variant="default" placeholder="input" ref={drag} />
+  )
+}
+
+function MockRow({ item }) {
+  const [values, handlers] = useListState()
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "component",
+    item,
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+
+  const [{ isOver }, drop] = useDrop({
+    accept: "component",
+    collect: monitor => ({
+      isOver: monitor.isOver({ shallow: true }),
+    }),
+    canDrop: (item, monitor) => monitor.isOver({ shallow: true }),
+    drop: (item) => {
+      handlers.append(item)
+    }
+  })
+
+  return (
+    <Group ref={(node) => drag(drop(node))} spacing="xs" p={4} noWrap sx={{ minHeight: 36, minWidth: 36, border: isOver ? '2px solid blue' : '1px solid #eee', overflowX: 'auto' }}>
+      {
+        values.map((item, i) => {
+          const MockComponent = mocks[item.component]
+          return <MockComponent key={i} item={item} />
+        })
+      }
+    </Group>
+  )
+}
+
+function MockColumn({ item }) {
+  const [values, handlers] = useListState()
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "component",
+    item,
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+
+  const [{ isOver }, drop] = useDrop({
+    accept: "component",
+    collect: monitor => ({
+      isOver: monitor.isOver({ shallow: true }),
+    }),
+    canDrop: (item, monitor) => monitor.isOver({ shallow: true }),
+    drop: (item) => {
+      handlers.append(item)
+    }
+  })
+
+  return (
+    <Group ref={(node) => drag(drop(node))} direction="column" spacing="xs" p={4} noWrap sx={{ minHeight: 36, minWidth: 36, border: isOver ? '2px solid blue' : '1px solid #eee', overflowX: 'auto' }}>
+      {
+        values.map((item, i) => {
+          const MockComponent = mocks[item.component]
+          return <MockComponent key={i} item={item} />
+        })
+      }
+    </Group>
+  )
+}
--- frontend/src/components/Designer.jsx
+++ frontend/src/components/Designer.jsx
@@ -1,7 +1,10 @@
 import { Box, Grid, Group, Paper, Center } from "@mantine/core";
-import { useListState } from "@mantine/hooks";
+import { useId, useListState } from "@mantine/hooks";
 import { useEffect, useState } from "react";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import { components } from "../blocks/components";
+import { mocks } from "../blocks/mocks";
 
 const testlayout = [
   components.button,
@@ -26,15 +29,16 @@ const testlayout = [
 ]
 
 export default function () {
-  const [values, handlers] = useListState()
   const [selectedComponent, setSelectedComponent] = useState(null)
   return (
-    <Grid>
-      <Grid.Col span={2}><ComponentsPanel /></Grid.Col>
-      <Grid.Col span={6}><LayoutPanel /></Grid.Col>
-      <Grid.Col span={2}><TreePanel setSelectedComponent={setSelectedComponent} layout={testlayout} /></Grid.Col>
-      <Grid.Col span={2}><PropertiesPanel component={selectedComponent} /></Grid.Col>
-    </Grid>
+    <DndProvider backend={HTML5Backend}>
+      <Grid>
+        <Grid.Col span={2}><ComponentsPanel /></Grid.Col>
+        <Grid.Col span={6}><LayoutPanel /></Grid.Col>
+        <Grid.Col span={2}><TreePanel setSelectedComponent={setSelectedComponent} layout={testlayout} /></Grid.Col>
+        <Grid.Col span={2}><PropertiesPanel component={selectedComponent} /></Grid.Col>
+      </Grid>
+    </DndProvider>
   )
 }
 
@@ -45,9 +49,7 @@ function ComponentsPanel() {
       <Group direction="column" grow>
         {
           Object.keys(components).map((component, i) => (
-            <Box sx={{ padding: 8, backgroundColor: 'white', border: '1px solid #eee' }} key={i}>
-              {component}
-            </Box>
+            <ComponentPanelItem component={component} key={i} />
           ))
         }
       </Group>
@@ -55,11 +57,55 @@ function ComponentsPanel() {
   )
 }
 
+function ComponentPanelItem({ component }) {
+  const uuid = useId()
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "component",
+    item: {
+      component,
+    },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+
+  return (
+    <Box sx={{ padding: 8, backgroundColor: 'white', border: '1px solid #eee'}} ref={drag}>
+      {component}
+    </Box>
+  )
+}
+
 function LayoutPanel() {
+  const [values, handlers] = useListState()
+
+  useEffect(() => {
+    console.log('LayoutPanel', values)
+  }, [values])
+
+  const [{ isOver }, drop] = useDrop({
+    accept: "component",
+    collect: monitor => ({
+      isOver: monitor.isOver({ shallow: true }),
+    }),
+    canDrop: (item, monitor) => monitor.isOver({ shallow: true }),
+    drop: (item) => {
+      handlers.append(item)
+    }
+  })
+
   return (
     <Center>
-      <Paper sx={{ height: 540, width: 320 }} shadow="xs" withBorder>
-        test
+      <Paper sx={{ height: 540, width: 320 }} shadow="xs" withBorder ref={drop}>
+        <Group direction="column">
+          {
+            values.map((item, i) => {
+              const MockComponent = mocks[item.component]
+              return <MockComponent key={i} item={item} />
+            })
+          }
+        </Group>
       </Paper>
     </Center>
   )
@@ -70,7 +116,7 @@ function NonTerminal({ item, level, setSelectedComponent }) {
     return <div onClick={(e) => { e.preventDefault(); e.stopPropagation(); setSelectedComponent(item) }}>{item.name}</div>
   }
   return (
-    <div onClick={(e) => {e.preventDefault(); e.stopPropagation(); setSelectedComponent(item)}}>
+    <div onClick={(e) => { e.preventDefault(); e.stopPropagation(); setSelectedComponent(item) }}>
       {item.name}
       <Box sx={{ paddingLeft: '1rem' }}>
         {
@@ -97,9 +143,6 @@ function TreePanel({ layout, setSelectedComponent }) {
 }
 
 function PropertiesPanel({ component }) {
-  useEffect(() => {
-    console.log(component)
-  }, [component])
   if (!component) {
     return "select a component";
   }
