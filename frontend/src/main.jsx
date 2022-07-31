1ba7081d3ece8c4d445eada53eb4221a08de4da7
Pavitra Golchha <hello@pavi2410.me>
2022-07-31 22:57:14 +0530

    React 18

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
