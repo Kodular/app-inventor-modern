caf6b512eb59051c318f3eaf66b95b31523bf0d7
Pavitra Golchha <hello@pavi2410.me>
2022-08-21 16:13:10 +0530

    Typescript + Eslint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from "path";
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 })
