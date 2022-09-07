7fc41e59e178607cc896c99e187e1c887174f59e
Pavitra Golchha <hello@pavi2410.me>
2022-09-07 23:22:47 +0530

    Add global styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { MantineProvider } from '@mantine/core'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <App/>
+      </MantineProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
