201d2ca0da370c64dcef861fc7094e9924486c56
Pavitra Golchha <pavitra@kodular.io>
2022-10-27 01:53:16 +0530

    Redesign project card

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,27 +1,32 @@
-import { Card, Image, Text, Badge, Button, Group, Avatar, useMantineTheme } from '@mantine/core';
-import { Link } from 'react-router-dom';
+import { Avatar, Group, Paper, Text, useMantineTheme } from "@mantine/core"
+import { Link } from "react-router-dom"
+import logo from "@/logo.png"
 
-export default function ProjectCard({project}) {
-  const theme = useMantineTheme();
+export default function ProjectCard ({ project }) {
+  const theme = useMantineTheme()
 
-  const secondaryColor = theme.colorScheme === 'dark'
+  const secondaryColor = theme.colorScheme === "dark"
     ? theme.colors.dark[1]
-    : theme.colors.gray[7];
+    : theme.colors.gray[7]
 
   return (
-    <Card shadow="sm" padding="lg">
-      <Group style={{ marginBottom: 8, marginTop: theme.spacing.sm }}>
-        <Avatar src={null} alt="no image here" color="indigo" />
-        <Text weight={600}>{project.name}</Text>
+    <Paper withBorder p="lg" radius="md" component={Link} to={"/project/" + project.id} sx={{
+      '&:hover': {
+        borderColor: 'lightblue'
+      }
+    }}>
+      <Group>
+        <Avatar size="lg" radius="md" style={{ backgroundColor: "lightblue" }} src={logo} alt="no image here"/>
+        <div>
+          <Text weight={600}>{project.name}</Text>
+          <Text size="sm" style={{
+            color: secondaryColor,
+            lineHeight: 1.5
+          }}>
+            {project.description}
+          </Text>
+        </div>
       </Group>
-
-      <Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
-        {project.description}
-      </Text>
-
-      <Button variant="light" color="blue" fullWidth style={{ marginTop: 14 }} component={Link} to={"/project/" + project.id}>
-        Edit project
-      </Button>
-    </Card>
+    </Paper>
   )
 }
