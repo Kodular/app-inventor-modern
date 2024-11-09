df5fbf1218f3c2be369658baa09a13bcc49d2086
Pavitra Golchha <hello@pavi2410.me>
2024-11-09 10:43:44 +0530

    fix Main page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,13 @@
-import { AppShellHeader, Avatar, Group, Title } from "@mantine/core"
+import { Avatar, Group, Title } from "@mantine/core"
 import logo from "@/logo.png"
 
 export default function () {
   return (
-    <AppShellHeader height={60} padding="xs">
+    <>
       <Group>
         <Avatar src={logo} alt="logo" color="green" />
         <Title order={4}>App Inventor Modern</Title>
       </Group>
-    </AppShellHeader>
+    </>
   )
 }
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -4,62 +4,67 @@ import ProjectCard from "@/components/ProjectCard"
 import Header from "@/components/Header"
 import { useState } from "react"
 
-export default function Main () {
+export default function Main() {
   const [viewType, setViewType] = useState("grid")
 
   return (
     <AppShell
       padding="md"
-      header={<Header/>}
+      header={{ height: 48 }}
       styles={(theme) => ({
         main: { backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0] },
       })}
     >
-      <Stack>
-        <Group position="apart">
-          <Group>
-            <Button variant="light">
-              Start new project
-            </Button>
-            <Button variant="light">
-              Import project
-            </Button>
+      <AppShell.Header>
+        <Header />
+      </AppShell.Header>
+      <AppShell.Main>
+        <Stack>
+          <Group position="apart">
+            <Group>
+              <Button variant="light">
+                Start new project
+              </Button>
+              <Button variant="light">
+                Import project
+              </Button>
+            </Group>
+            <SegmentedControl
+              value={viewType}
+              onChange={setViewType}
+              data={["grid", "table"]}
+            />
           </Group>
-          <SegmentedControl
-            value={viewType}
-            onChange={setViewType}
-            data={["grid", "table"]}
-          />
-        </Group>
 
-        <ProjectsView viewType={viewType}/>
-      </Stack>
+          <ProjectsView viewType={viewType} />
+        </Stack>
+      </AppShell.Main>
     </AppShell>
   )
 }
 
-function ProjectsView ({ viewType }) {
+function ProjectsView({ viewType }) {
 
   if (viewType === "table") {
     return (
       <Table highlightOnHover>
         <thead>
-        <tr>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Action</th>
-        </tr>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Action</th>
+          </tr>
         </thead>
         <tbody>
-        {
-          projects.map((project, i) => (
-            <tr key={i}>
-              <td>{project.name}</td>
-              <td>{project.description}</td>
-              <td><Button variant="subtle">Edit</Button></td>
-            </tr>
-          ))
-        }
+          {
+            projects.map((project, i) => (
+              <tr key={i}>
+                <td>{project.name}</td>
+                <td>{project.description}</td>
+                <td><Button variant="subtle">Edit</Button></td>
+              </tr>
+            ))
+          }
         </tbody>
       </Table>
     )
@@ -69,7 +74,7 @@ function ProjectsView ({ viewType }) {
     <SimpleGrid cols={6}>
       {
         projects.map((project, i) => (
-          <ProjectCard project={project} key={i}/>
+          <ProjectCard project={project} key={i} />
         ))
       }
     </SimpleGrid>
